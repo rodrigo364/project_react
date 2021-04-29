662f1c716c4c75a9315e677041f07f24253cdd18
Rodrigo Liz <rodrigo.liz@calcard.com.br>
2021-04-29 07:53:54 -0300

    build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import App from './App'
 // import App from './adapter/Api'
 
 
+ function formatName(user) {
+     return user.firstName + ' '+user.lastName;
+ }
+
+ const user = {
+    firstName: 'Helper',
+    lastName: 'PErez'
+ }
 
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(
+    <h1>Hello , {formatName(user)}</h1>,
+    document.getElementById('root')
+)
+
+// ReactDOM.render(<App/>,document.getElementById('root'));
